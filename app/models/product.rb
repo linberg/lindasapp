a39308e0d8b0db456beb42240906ee7d29fc6351
Linda <lindabergen@gmail.com>
2016-04-25 20:49:06 +0200

    added product reviews and raty functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   validates :name, :description, :image_url, :color, :price, presence: true
   has_many :orders
   has_many :comments
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
 end
