882982f0e8d231ee7f8d1b030a4e55bf18230131
Linda <lindabergen@gmail.com>
2016-05-04 21:28:52 +0200

    pagination in comments fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all, User, id: user.id
+    else
+      can :read, :all
+    end
   end
 end
     # Define abilities for the passed in user here. For example:
