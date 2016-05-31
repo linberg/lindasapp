e7be0594500bc819c110f7d536265adb3936754e
Linda <lindabergen@gmail.com>
2016-05-31 21:43:01 +0200

    added elevateZoom jQuery plug-in

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,11 @@
 $(document).on('ready page:load', function(){
+    $('.img-zoom').elevateZoom({
+        zoomType: "lens",
+        lensShape: "round",
+        lensSize: 150
+
+      });
+
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
