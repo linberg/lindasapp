5a28389c9adfa9de4e22644acab6f8585a8eb02e
Linda <lindabergen@gmail.com>
2016-06-17 17:24:10 +0200

    redis installation and implementation

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Lindasapp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.cache_store = :redis_store, 'redis://localhost:6370/0/cache'
   end
 end
