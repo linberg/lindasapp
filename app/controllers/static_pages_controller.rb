1df2d30a3e374509570b459dadd0dfd5262fb9f6
Linda <lindabergen@gmail.com>
2016-07-06 15:01:41 +0200

    controller user authentication

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,15 @@ class StaticPagesController < ApplicationController
     @products = Product.all
   end
 
+  def thank_you
+    @name = params[:name]
+    @email= params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+    # ActionMailer::Base.mail(:from => @email,
+    #  :to => 'alexwen35@gmail.com',
+    #  :subject => "A new contact form message from #{@name}",
+    #  :body => @message).deliver_now
+  end
+  
 end
