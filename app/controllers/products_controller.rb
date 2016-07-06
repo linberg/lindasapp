5219ba27940f1fa00b2903d34e6761cabbc9e0b3
Linda <lindabergen@gmail.com>
2016-07-06 10:58:28 +0200

    product form styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    @product = Product.new
+    @product = Product.new(params[:product])
   end
 
   # GET /products/1/edit
