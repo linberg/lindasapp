3002fed566f608605346d07fb95e4e36f81bc554
Linda <lindabergen@gmail.com>
2016-07-30 11:06:08 +0200

    search form fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      @products = Product.where("name ILIKE ?", "%#{search_term}%")
       #return filtered list here
     else
       @products = Product.all
